09f9db8d885adf2731720249fe392de225f1499e
Reinan <reinangabriel1520@gmail.com>
2021-10-23 19:07:46 -0300

    feat: adiciona novos controladores para as rotas

---
+++ app/Http/Controllers/Web/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class PageController extends Controller
+{
+    /**
+     * Método para exibir a página inicial do site.
+     * @return Response
+     */
+    public function welcome(): Response
+    {
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
