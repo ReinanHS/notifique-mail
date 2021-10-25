12a2f51be294c7276460d02d7e130b13c4287da2
Reinan <reinangabriel1520@gmail.com>
2021-10-25 03:31:56 -0300

    refactor: adiciona um try/catch no sendSMS

--- app/Observers/Mail/MailObserver.php
+++ app/Observers/Mail/MailObserver.php
@@ -18,22 +18,24 @@ class MailObserver
      */
     public function created(Mail $mail): void
     {
-        //MailFacade::to($mail->userMailTo)->send(new SendMailContent($mail));
+        MailFacade::to($mail->userMailTo)->send(new SendMailContent($mail));
         $this->sendSMS($mail);
     }
 
     /**
      * Método para enviar um SMS.
      * @param Mail $mail
-     * @throws ConfigurationException
-     * @throws TwilioException
      */
     private function sendSMS(Mail $mail): void
     {
-        $client = new Client(env('TWILIOO_ACCOUNT_SID'), env('TWILIOO_AUTH_TOKEN'));
-        $client->messages->create($mail->userMailFrom->phone_number, [
-            'from' => env('TWILIOO_PHONE_NUMBER'),
-            'body' => 'Olá ' . $mail->userMailTo->name . ', você acabou de receber uma mensagem de ' . $mail->userMailFrom->name . '. Para mais informações acesse o site https://mail.reinanhs.com/',
-        ]);
+        try{
+            $client = new Client(env('TWILIOO_ACCOUNT_SID'), env('TWILIOO_AUTH_TOKEN'));
+            $client->messages->create($mail->userMailFrom->phone_number, [
+                'from' => env('TWILIOO_PHONE_NUMBER'),
+                'body' => 'Olá ' . $mail->userMailTo->name . ', você acabou de receber uma mensagem de ' . $mail->userMailFrom->name . '. Para mais informações acesse o site https://mail.reinanhs.com/',
+            ]);
+        } catch (\Exception $exception){
+            // TODO: Realizar implementação para tratamentos de erros
+        }
     }
 }
